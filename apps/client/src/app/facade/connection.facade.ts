48f16eee6e91f1964812f31a21bdd3c96f218cc9
Calin C <calin.chiper@tss-yonder.com>
2023-10-08 17:21:20 +0300

    wrap alertservice as component store effect

--- apps/client/src/app/facade/connection.facade.ts
+++ apps/client/src/app/facade/connection.facade.ts
@@ -1,10 +1,10 @@
 import { Inject, Injectable } from '@angular/core';
 import { ConnectionService } from '../service/connection.service';
-import { TuiAlertService } from '@taiga-ui/core';
+import { TuiAlertOptions, TuiAlertService } from '@taiga-ui/core';
 import { ConnectionListItem, NewConnection } from '@letyca/contracts';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
 import { HttpErrorResponse } from '@angular/common/http';
-import { tap, exhaustMap } from 'rxjs';
+import { tap, exhaustMap, switchMap } from 'rxjs';
 
 export type ConnectionState = {
   loading: boolean;
@@ -13,14 +13,16 @@ export type ConnectionState = {
 
 @Injectable()
 export class ConnectionFacade extends ComponentStore<ConnectionState> {
+  readonly connections = this.selectSignal((state) => state.connections);
+  readonly loading = this.selectSignal((state) => state.loading);
+
   constructor(
     private service: ConnectionService,
     @Inject(TuiAlertService) private alertService: TuiAlertService
   ) {
     super({ loading: false, connections: [] });
   }
-  readonly connections$ = this.select((state) => state.connections);
-  readonly loading$ = this.select((state) => state.loading);
+
   readonly refresh = this.effect<void>((trigger$) =>
     trigger$.pipe(
       tap(() => this.patchState({ loading: true })),
@@ -35,39 +37,54 @@ export class ConnectionFacade extends ComponentStore<ConnectionState> {
       )
     )
   );
+
   readonly add = this.effect<NewConnection>((newConnection$) =>
     newConnection$.pipe(
       exhaustMap((newConnection) => this.service.create(newConnection)),
       tapResponse(
         () => {
-          this.alertService
-            .open('The connection was successfully added', {
+          this.displayAlert({
+            content: 'The connection was successfully added',
+            options: {
               label: 'Connection Added',
               status: 'success',
               icon: 'tuiIconCheckCircleLarge',
-            })
-            .subscribe();
+            },
+          });
           this.refresh();
         },
         (error: HttpErrorResponse) => console.error(error)
       )
     )
   );
+
   readonly delete = this.effect<string>((connectionId$) =>
     connectionId$.pipe(
       exhaustMap((connectionId) => this.service.deleteById(connectionId)),
       tapResponse(
         () => {
-          this.alertService
-            .open('The connection was successfully deleted', {
+          this.displayAlert({
+            content: 'The connection was successfully deleted',
+            options: {
               label: 'Connection Deleted',
               status: 'success',
               icon: 'tuiIconCheckCircleLarge',
-            })
-            .subscribe();
+            },
+          });
           this.refresh();
         },
         (error: HttpErrorResponse) => console.error(error)
+      )
+    )
+  );
+
+  readonly displayAlert = this.effect<{
+    content: string;
+    options: Partial<TuiAlertOptions<void>>;
+  }>((options$) =>
+    options$.pipe(
+      switchMap(({ content, options }) =>
+        this.alertService.open(content, options)
       )
     )
   );
