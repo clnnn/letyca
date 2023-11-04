fc8fd0ec9152fddb0be7cadc473fbe5aba65d38a
Calin C <calin.chiper@tss-yonder.com>
2023-11-04 15:43:04 +0000

    Inject data using inject() instead of constructor and use takeUntilDestroyed

--- apps/client/src/app/components/action-cell-renderer/action-cell-renderer.component.ts
+++ apps/client/src/app/components/action-cell-renderer/action-cell-renderer.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
 import { ConnectionFacade } from '../../facade/connection.facade';
 import { TuiButtonModule } from '@taiga-ui/core/components/button';
 
 @Component({
-    selector: 'le-action-cell-renderer',
-    templateUrl: './action-cell-renderer.component.html',
-    styleUrls: ['./action-cell-renderer.component.scss'],
-    standalone: true,
-    imports: [TuiButtonModule],
+  selector: 'le-action-cell-renderer',
+  templateUrl: './action-cell-renderer.component.html',
+  styleUrls: ['./action-cell-renderer.component.scss'],
+  standalone: true,
+  imports: [TuiButtonModule],
 })
 export class ActionCellRendererComponent implements ICellRendererAngularComp {
-  connectionId?: string;
+  private readonly facade = inject(ConnectionFacade);
 
-  constructor(private facade: ConnectionFacade) {}
+  private connectionId?: string;
 
   agInit(params: ICellRendererParams): void {
     this.connectionId = params.node.id;
--- apps/client/src/app/components/connections-page/connections-page.component.ts
+++ apps/client/src/app/components/connections-page/connections-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, inject, DestroyRef } from '@angular/core';
 import { TuiDialogService } from '@taiga-ui/core';
 import { PolymorpheusComponent } from '@tinkoff/ng-polymorpheus';
 import { CreateEditConnectionDialogContentComponent } from '../create-edit-connection-dialog-content/create-edit-connection-dialog-content.component';
@@ -6,24 +6,24 @@ import { ConnectionFacade } from '../../facade/connection.facade';
 import { NewConnection } from '@letyca/contracts';
 import { ConnectionsGridComponent } from '../connections-grid/connections-grid.component';
 import { ConnectionsPageHeaderComponent } from '../connections-page-header/connections-page-header.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'le-connections-page',
-    templateUrl: './connections-page.component.html',
-    styleUrls: ['./connections-page.component.scss'],
-    standalone: true,
-    imports: [ConnectionsPageHeaderComponent, ConnectionsGridComponent],
+  selector: 'le-connections-page',
+  templateUrl: './connections-page.component.html',
+  styleUrls: ['./connections-page.component.scss'],
+  standalone: true,
+  imports: [ConnectionsPageHeaderComponent, ConnectionsGridComponent],
 })
 export class ConnectionsPageComponent implements OnInit {
+  private readonly facade = inject(ConnectionFacade);
+  private readonly dialogService = inject(TuiDialogService);
+  private readonly injector = inject(Injector);
+  private readonly destroy = inject(DestroyRef);
+
   readonly pageTitle = 'Connections';
   readonly connections = this.facade.connections;
   readonly loading = this.facade.loading;
-
-  constructor(
-    private facade: ConnectionFacade,
-    @Inject(TuiDialogService) private readonly dialogService: TuiDialogService,
-    @Inject(Injector) private readonly injector: Injector
-  ) {}
 
   ngOnInit(): void {
     this.facade.refresh();
@@ -41,6 +41,7 @@ export class ConnectionsPageComponent implements OnInit {
           dismissible: true,
         }
       )
+      .pipe(takeUntilDestroyed(this.destroy))
       .subscribe((data) => {
         this.facade.add(data);
       });
