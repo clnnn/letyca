3a9fd41d83c753acb5d1984bc1314418eb8f3a5a
Calin C <calin.chiper@tss-yonder.com>
2023-11-05 14:20:50 +0000

    Remove unused module/components imports in AppModule  and create chart facade

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,18 +1,5 @@
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import {
-  TuiRootModule,
-  TuiDialogModule,
-  TuiAlertModule,
-  TUI_SANITIZER,
-  TuiTextfieldControllerModule,
-  TuiHostedDropdownModule,
-  TuiButtonModule,
-  TuiDataListModule,
-  TuiSvgModule,
-  TuiErrorModule,
-  TuiLoaderModule,
-} from '@taiga-ui/core';
-import { TuiTableModule } from '@taiga-ui/addon-table';
+import { TuiRootModule, TUI_SANITIZER } from '@taiga-ui/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -22,71 +9,25 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ConnectionService } from './service/connection.service';
 import { ConnectionFacade } from './facade/connection.facade';
-import { ConnectionsGridComponent } from './components/connections-grid/connections-grid.component';
 import { PageComponent } from './components/page/page.component';
-import {
-  TuiAvatarModule,
-  TuiFieldErrorPipeModule,
-  TuiInputModule,
-  TuiInputNumberModule,
-  TuiInputPasswordModule,
-  TuiIslandModule,
-  TuiTabsModule,
-} from '@taiga-ui/kit';
-import { TuiBlockStatusModule } from '@taiga-ui/layout';
-import { AgGridModule } from 'ag-grid-angular';
-import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowsePageComponent } from './components/browse-page/browse-page.component';
-import { ConnectionsPageComponent } from './components/connections-page/connections-page.component';
-import { ConnectionsPageHeaderComponent } from './components/connections-page-header/connections-page-header.component';
-import { ActionCellRendererComponent } from './components/action-cell-renderer/action-cell-renderer.component';
-import { CreateEditConnectionDialogContentComponent } from './components/create-edit-connection-dialog-content/create-edit-connection-dialog-content.component';
+import { ChartFacade } from './facade/chart.facade';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        TuiRootModule,
-        TuiDialogModule,
-        TuiAlertModule,
-        TuiTableModule,
-        TuiTabsModule,
-        TuiInputModule,
-        TuiInputNumberModule,
-        TuiTextfieldControllerModule,
-        TuiAvatarModule,
-        TuiHostedDropdownModule,
-        TuiButtonModule,
-        TuiDataListModule,
-        TuiSvgModule,
-        TuiIslandModule,
-        TuiBlockStatusModule,
-        AgGridModule,
-        TuiFieldErrorPipeModule,
-        TuiErrorModule,
-        TuiInputPasswordModule,
-        TuiAlertModule,
-        TuiLoaderModule,
-        ConnectionsGridComponent,
-        PageComponent,
-        DashboardPageComponent,
-        BrowsePageComponent,
-        ConnectionsPageComponent,
-        ConnectionsPageHeaderComponent,
-        ActionCellRendererComponent,
-        CreateEditConnectionDialogContentComponent,
-    ],
-    providers: [
-        ConnectionService,
-        ConnectionFacade,
-        { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
-    ],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    PageComponent,
+  ],
+  providers: [
+    ConnectionService,
+    ConnectionFacade,
+    ChartFacade,
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
