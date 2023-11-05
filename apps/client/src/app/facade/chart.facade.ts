1f378b606bbcb4ac527e137364f3676fece7e8eb
Calin C <calin.chiper@tss-yonder.com>
2023-11-05 14:39:51 +0000

    remove unused import

--- apps/client/src/app/facade/chart.facade.ts
+++ apps/client/src/app/facade/chart.facade.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { ChartResponse } from '@letyca/contracts';
+
+type ChartState = {
+  chart?: ChartResponse;
+  loading: boolean;
+};
 
 @Injectable()
 export class ChartFacade {}
