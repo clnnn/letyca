0fa3a0968fb741d21ae663cb36df46b2fdfc171c
Calin C <calin.chiper@tss-yonder.com>
2023-11-05 06:06:55 +0000

    correct routing

--- apps/client/src/app/components/browse-page/browse-page.component.ts
+++ apps/client/src/app/components/browse-page/browse-page.component.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 import { TuiSvgModule } from '@taiga-ui/core/components/svg';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TuiTabsModule, TuiIslandModule } from '@taiga-ui/kit';
+import { TuiLinkModule } from '@taiga-ui/core';
 
 @Component({
-    selector: 'le-browse-page',
-    templateUrl: './browse-page.component.html',
-    styleUrls: ['./browse-page.component.scss'],
-    standalone: true,
-    imports: [
-        TuiTabsModule,
-        RouterLink,
-        RouterLinkActive,
-        TuiSvgModule,
-        TuiIslandModule,
-        RouterOutlet,
-    ],
+  selector: 'le-browse-page',
+  templateUrl: './browse-page.component.html',
+  styleUrls: ['./browse-page.component.scss'],
+  standalone: true,
+  imports: [
+    TuiTabsModule,
+    TuiLinkModule,
+    RouterLink,
+    RouterLinkActive,
+    TuiSvgModule,
+    TuiIslandModule,
+    RouterOutlet,
+  ],
 })
 export class BrowsePageComponent {}
