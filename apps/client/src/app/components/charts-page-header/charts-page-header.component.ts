4393afd41092a5c88501abd700994350084f84f3
Calin C <calin.chiper@tss-yonder.com>
2023-11-05 07:06:57 +0000

    Select connection in charts page

--- apps/client/src/app/components/charts-page-header/charts-page-header.component.ts
+++ apps/client/src/app/components/charts-page-header/charts-page-header.component.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  TuiComboBoxModule,
+  TuiDataListWrapperModule,
+  TuiStringifyContentPipeModule,
+} from '@taiga-ui/kit';
+import { TuiDataListModule } from '@taiga-ui/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ConnectionListItem } from '@letyca/contracts';
+import { TuiValueChangesModule } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'le-charts-page-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TuiComboBoxModule,
+    TuiDataListModule,
+    TuiDataListWrapperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiStringifyContentPipeModule,
+    TuiValueChangesModule,
+  ],
   templateUrl: './charts-page-header.component.html',
   styleUrls: ['./charts-page-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,4 +36,21 @@ import { CommonModule } from '@angular/common';
 export class ChartsPageHeaderComponent {
   @Input({ required: true })
   title!: string;
+
+  @Input({ required: true })
+  connections!: ConnectionListItem[];
+
+  @Output()
+  readonly connectionChange = new EventEmitter<ConnectionListItem>();
+
+  connectionDropdown = new FormControl<ConnectionListItem | undefined>(
+    undefined
+  );
+
+  readonly stringify = (item: ConnectionListItem): string =>
+    `${item.host}:${item.port} - ${item.database}`;
+
+  onChanges(selected?: ConnectionListItem): void {
+    this.connectionChange.emit(selected);
+  }
 }
--- apps/client/src/app/components/charts-page/charts-page.component.ts
+++ apps/client/src/app/components/charts-page/charts-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsPageHeaderComponent } from '../charts-page-header/charts-page-header.component';
 import { ConnectionFacade } from '../../facade/connection.facade';
+import { ConnectionListItem } from '@letyca/contracts';
 
 @Component({
   selector: 'le-charts-page',
@@ -10,9 +11,19 @@ import { ConnectionFacade } from '../../facade/connection.facade';
   templateUrl: './charts-page.component.html',
   styleUrls: ['./charts-page.component.scss'],
 })
-export class ChartsPageComponent {
+export class ChartsPageComponent implements OnInit {
   private readonly facade = inject(ConnectionFacade);
 
   readonly pageTitle = 'Charts';
   readonly connections = this.facade.connections;
+
+  selectedConnection?: ConnectionListItem;
+
+  ngOnInit(): void {
+    this.facade.refresh();
+  }
+
+  connectionChange(item?: ConnectionListItem): void {
+    this.selectedConnection = item;
+  }
 }
