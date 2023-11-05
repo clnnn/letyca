501528a3365f70faede9b41d97e8241c1cdaf71c
Calin C <calin.chiper@tss-yonder.com>
2023-11-05 13:19:27 +0000

    Add chart preview component to charts pages

--- apps/client/src/app/components/charts-page/charts-page.component.ts
+++ apps/client/src/app/components/charts-page/charts-page.component.ts
@@ -3,27 +3,32 @@ import { CommonModule } from '@angular/common';
 import { ChartsPageHeaderComponent } from '../charts-page-header/charts-page-header.component';
 import { ConnectionFacade } from '../../facade/connection.facade';
 import { ConnectionListItem } from '@letyca/contracts';
+import { ChartPreviewComponent } from '../chart-preview/chart-preview.component';
 
 @Component({
   selector: 'le-charts-page',
   standalone: true,
-  imports: [CommonModule, ChartsPageHeaderComponent],
+  imports: [CommonModule, ChartsPageHeaderComponent, ChartPreviewComponent],
   templateUrl: './charts-page.component.html',
   styleUrls: ['./charts-page.component.scss'],
 })
 export class ChartsPageComponent implements OnInit {
-  private readonly facade = inject(ConnectionFacade);
+  private readonly connectionFacade = inject(ConnectionFacade);
 
   readonly pageTitle = 'Charts';
-  readonly connections = this.facade.connections;
+  readonly connections = this.connectionFacade.connections;
 
   selectedConnection?: ConnectionListItem;
 
   ngOnInit(): void {
-    this.facade.refresh();
+    this.connectionFacade.refresh();
   }
 
   connectionChange(item?: ConnectionListItem): void {
     this.selectedConnection = item;
+  }
+
+  messageSubmit(message: string): void {
+    console.log(message);
   }
 }
