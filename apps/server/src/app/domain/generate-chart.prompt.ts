557caabd308d3f07391d820bef2860ccb52d31a2
Calin C <calin.chiper@tss-yonder.com>
2023-11-12 07:45:53 +0000

    remove unused prompt

--- apps/server/src/app/domain/generate-chart.prompt.ts
+++
@@ -1,8 +0,0 @@
-import { PromptTemplate } from 'langchain/prompts';
-
-export const generateChartPrompt =
-  PromptTemplate.fromTemplate(`Based on the table schema below, Question, SQL Query, SQL Response answer the question as best as possible.\n{formatInstructions}:
-      {schema}
-      Question: {question}
-      SQL Query: {query}
-      SQL Response: {response}`);
