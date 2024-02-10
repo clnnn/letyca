e8c205f637f4f02ae198a5cf61953e12cfac7ab6
Calin C <calin.chiper@tss-yonder.com>
2024-02-10 05:50:10 +0000

    add demo mode interceptor on the server

--- apps/server/src/app/controller/connection.controller.ts
+++ apps/server/src/app/controller/connection.controller.ts
@@ -1,12 +1,22 @@
 import { ConnectionListItem, NewConnection } from '@letyca/contracts';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PrismaService } from '../data-access/prisma.service';
+import { DemoModeInterceptor } from '../interceptor/demo-mode.interceptor';
 
 @Controller('connections')
 export class ConnectionController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Post()
+  @UseInterceptors(DemoModeInterceptor)
   async create(@Body() newConnection: NewConnection): Promise<void> {
     await this.prisma.connection.create({ data: newConnection });
   }
@@ -25,6 +35,7 @@ export class ConnectionController {
   }
 
   @Delete(':id')
+  @UseInterceptors(DemoModeInterceptor)
   async deleteById(@Param('id') id: string): Promise<void> {
     await this.prisma.connection.delete({
       where: {
