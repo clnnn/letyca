7e71765b457ee05760623c337b3e7329fba87ceb
Calin C <calin.chiper@tss-yonder.com>
2024-02-29 14:29:55 +0000

    hardcoded examples on northwind

--- apps/client/src/app/components/chart-prompt/chart-prompt.component.ts
+++ apps/client/src/app/components/chart-prompt/chart-prompt.component.ts
@@ -28,7 +28,7 @@ import {
   TuiTextfieldControllerModule,
 } from '@taiga-ui/core';
 import { LoadingState } from '../../utils';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 
 @Component({
@@ -57,11 +57,48 @@ export class ChartPromptComponent implements OnInit, OnChanges {
   @Input({ required: true })
   chartLoading?: LoadingState;
 
-  @Input({ required: true })
-  examples!: { name: string; query: string }[];
-
   @Output()
   readonly humanQuerySubmit = new EventEmitter<string>();
+
+  @Input({ required: true })
+  demoMode!: boolean;
+
+  readonly demoExamples: { name: string; query: string }[] = [
+    {
+      name: 'Total number of products',
+      query: 'Total number of products',
+    },
+    {
+      name: 'Total number of products by type',
+      query:
+        'Please display the number of total products by types in a pie chart',
+    },
+    {
+      name: 'Filter products by price',
+      query:
+        'Number of products with price below 10 and products with price over 10 in a pie chart',
+    },
+    {
+      name: 'Monthly sales',
+      query: 'Represent monthly sales for year 1997 using a line chart',
+    },
+    {
+      name: 'Daily Cumulative sales',
+      query: 'cumulative revenue for December 1996 on daily basis',
+    },
+    {
+      name: 'Employees average age',
+      query: 'verage age of employees per country',
+    },
+    {
+      name: 'Top 5 customers',
+      query: 'Top 5 customers by revenue',
+    },
+    {
+      name: 'Products sorted by price',
+      query: 'from most expensive to most cheapest product, in a bar chart',
+    },
+  ];
 
   form = this.fb.group({
     humanQuery: this.fb.control<string>('', Validators.required),
--- apps/server/src/app/data-access/prisma.service.ts
+++ apps/server/src/app/data-access/prisma.service.ts
@@ -1,48 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from 'prisma/prisma-client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor(private configService: ConfigService) {
+  constructor() {
     super();
   }
 
   async onModuleInit() {
     await this.$connect();
-
-    const demoMode = this.configService.get<string>('DEMO_MODE');
-    if (demoMode === 'true') {
-      this.createNorthwindConnection();
-    }
-  }
-
-  private async createNorthwindConnection() {
-    await this.connection.deleteMany({ where: { host: 'northwind' } });
-    await this.connection.create({
-      data: {
-        host: 'northwind',
-        port: 5432,
-        database: 'northwind',
-        schema: 'public',
-        username: 'postgres',
-        password: 'postgres',
-        QueryExample: {
-          createMany: {
-            data: [
-              {
-                name: 'Total number of products',
-                query: 'Total number of products',
-              },
-              {
-                name: 'Total number of products by type',
-                query:
-                  'Please display the number of total products by types in a pie chart',
-              },
-            ],
-          },
-        },
-      },
-    });
   }
 }
