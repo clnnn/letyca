1ccf3810232d907a4c53165751cbcdb8c13eebb6
Calin C <calin.chiper@tss-yonder.com>
2024-04-07 17:22:07 +0000

    separate services for query generation, execution

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import OpenAI from 'openai';
 import { ConnectionController } from './controller/connection.controller';
-import { ChartController } from './controller/chart.controller';
 import { PrismaService } from './data-access/prisma.service';
-import { DatabaseService } from './service/database.service';
+import { QueryService } from './service/query.service';
 import { ChartService } from './service/chart.service';
 import { FeatureFlagController } from './controller/feature-flag.controller';
+import { ExecutionService } from './service/execution.service';
+import { ChartController } from './controller/chart.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [ConnectionController, ChartController, FeatureFlagController],
   providers: [
     PrismaService,
-    DatabaseService,
+    QueryService,
+    ExecutionService,
     ChartService,
     {
       provide: OpenAI,
