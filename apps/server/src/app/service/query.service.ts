80aee8910c5ed18e389ca369e8c38ae05e72c0d2
Calin C <calin.chiper@tss-yonder.com>
2024-04-08 11:57:54 +0000

    Modify the prompt few-shot examples so the alias for each table appears everytime

--- apps/server/src/app/service/query.service.ts
+++ apps/server/src/app/service/query.service.ts
@@ -40,19 +40,17 @@ export class QueryService {
         {
           role: 'system',
           content: `You are PostgreSQL Generator assistant expert in generating PostgreSQL queries based on this schema [[${schema}]]. Please answer the user question using just a SQL Query and nothing else. If it is possible, try to get human readable data from tables that can be represented in a chart.
-
           If user request a query that may generate a single row, single column value, use column name 'value' as alias. If user request data that can be represented in a bar chart, line chart or pie chart and the query select multiple columns then always use column names 'label' and 'value' as aliases.
           Examples:
           Example #1
           user: "total number of products"
-          assistant: "SELECT COUNT(*) AS value FROM products"
+          assistant: "SELECT COUNT(*) AS value FROM products p"
           Example #2
           user: "number of products by category"
-          assistant: "SELECT category AS label, COUNT(*) AS value FROM products JOIN categories ON products.category_id = categories.category_id GROUP BY category_name"
+          assistant: "SELECT c.category_name AS label, COUNT(*) AS value FROM products p JOIN categories c ON p.category_id = c.category_id GROUP BY c.category_name"
           Example #3
           user: "total sales by month"
-          assistant: "SELECT EXTRACT(MONTH FROM date) AS label, SUM(sales) AS value FROM sales GROUP BY EXTRACT(MONTH FROM date)"
-          `,
+          assistant: "SELECT EXTRACT(MONTH FROM s.date) AS label, SUM(s.sales) AS value FROM sales s GROUP BY EXTRACT(MONTH FROM s.date)"`,
         },
         {
           role: 'system',
