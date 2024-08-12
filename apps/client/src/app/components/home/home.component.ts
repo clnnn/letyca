3258c634699bbbea41f4fe95c1a1ea2a9a904a59
Calin C <calin.chiper@tss-yonder.com>
2024-08-12 06:30:17 +0000

    refactor: Update home component typewriter effect

--- apps/client/src/app/components/home/home.component.ts
+++ apps/client/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   concat,
   concatMap,
@@ -13,12 +13,18 @@ import {
   take,
 } from 'rxjs';
 
+type TypeParams = {
+  text: string;
+  speed: number;
+  backwards?: boolean;
+};
+
 @Component({
   selector: 'le-home',
   standalone: true,
   imports: [AsyncPipe],
   template: `
-    <span class="tui-text_h1">{{ t$ | async }}</span>
+    <span class="tui-text_h1">{{ typewriterText$ | async }}</span>
     <img
       src="/assets/images/welcome.svg"
       width="800"
@@ -29,58 +35,43 @@ import {
   styles: `
     :host {
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
       align-items: center;
-      flex-direction: column;
     }
   `,
 })
-export class HomeComponent implements OnInit {
-  titles = [
-    `Hi. I'm Letyca, your Analytical Copilot`,
-    'I am here to help you make sense of your data',
-    'I can help you with data analysis, visualization, and more',
-    'I am a proof of concept, so please be patient with me',
-  ];
+export class HomeComponent {
+  protected readonly typewriterText$ = this.typewriterEffect([
+    `Hi. I'm Letyca, your Analytical Copilot.`,
+    'I am here to help you make sense of your data.',
+    'I can help you with data analysis, visualization, and more.',
+    'I am a proof of concept, so please be patient with me.',
+  ]);
 
-  t$ = this.getTypewriterEffect(this.titles);
-
-  constructor() {}
-
-  ngOnInit(): void {
-    // Initialization code goes here
-  }
-
-  private type({ word, speed, backwards = false }: TypeParams) {
-    return interval(speed).pipe(
-      map((x) =>
-        backwards
-          ? word.substring(0, word.length - x)
-          : word.substring(0, x + 1)
-      ),
-      take(word.length)
+  private typewriterEffect(text: string[]) {
+    return from(text).pipe(
+      concatMap((text) => this.typeEffect(text)),
+      repeat()
     );
   }
 
-  typeEffect(word: string) {
+  private typeEffect(text: string) {
     return concat(
-      this.type({ word, speed: 40 }),
+      this.type({ text, speed: 40 }),
       of('').pipe(delay(1200), ignoreElements()),
-      this.type({ word, speed: 25, backwards: true }),
+      this.type({ text, speed: 25, backwards: true }),
       of('').pipe(delay(300), ignoreElements())
     );
   }
 
-  getTypewriterEffect(titles: string[]) {
-    return from(titles).pipe(
-      concatMap((title) => this.typeEffect(title)),
-      repeat()
+  private type({ text, speed, backwards = false }: TypeParams) {
+    return interval(speed).pipe(
+      map((x) =>
+        backwards
+          ? text.substring(0, text.length - x)
+          : text.substring(0, x + 1)
+      ),
+      take(text.length)
     );
   }
 }
-
-type TypeParams = {
-  word: string;
-  speed: number;
-  backwards?: boolean;
-};
