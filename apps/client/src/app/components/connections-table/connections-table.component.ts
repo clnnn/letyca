13315f36aa2fe6400a1558e47e49e1b80858f8d4
Calin C <calin.chiper@tss-yonder.com>
2024-08-13 08:29:02 +0000

    add WIP chip to connections table

--- apps/client/src/app/components/connections-table/connections-table.component.ts
+++ apps/client/src/app/components/connections-table/connections-table.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TuiTitle, TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TuiCell } from '@taiga-ui/layout';
 import { TuiTable } from '@taiga-ui/addon-table';
+import { TuiChip } from '@taiga-ui/kit';
+
 import { ConnectionListItem } from '@letyca/contracts';
 
 type Connection = ConnectionListItem;
 
-const tuiImports = [TuiCell, TuiTitle, TuiButton, TuiIcon, TuiTable];
+const tuiImports = [TuiCell, TuiTitle, TuiButton, TuiIcon, TuiTable, TuiChip];
 
 @Component({
   selector: 'le-connections-table',
