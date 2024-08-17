17473ffe1444d2b312e4c34efc9045eba659786c
Calin C <calin.chiper@tss-yonder.com>
2024-08-17 21:04:12 +0000

    chore: Update tsconfig to enable strict mode and add new npm dependencies

--- apps/server/src/app/service/query-parsing.service.spec.ts
+++ apps/server/src/app/service/query-parsing.service.spec.ts
@@ -253,7 +253,7 @@ describe('QueryParsingService', () => {
   describe('Grouping aggregation with functions as dimension columns (no alias)', () => {
     it('should return dimension function name and aggregation function name', () => {
       // given
-      const rawSQL = `SELECT date_trunc('month', o.order_date), SUM(od.quantity * p.unit_price) AS total_sales FROM orders o JOIN order_details od ON o.order_id = od.order_id JOIN products p ON od.product_id = p.product_id WHERE date_part('year', o.order_date) = 1997 AND p.category_id IN (SELECT category_id FROM products WHERE product_name ilike '%condiment%') GROUP BY date_trunc('month', o.order_date) ORDER BY MONTH`;
+      const rawSQL = `SELECT date_trunc('month', o.order_date), SUM(od.quantity * p.unit_price) FROM orders o JOIN order_details od ON o.order_id = od.order_id JOIN products p ON od.product_id = p.product_id WHERE date_part('year', o.order_date) = 1997 AND p.category_id IN (SELECT category_id FROM products WHERE product_name ilike '%condiment%') GROUP BY date_trunc('month', o.order_date) ORDER BY MONTH`;
 
       // when
       const result = service.parse(rawSQL);
@@ -262,8 +262,8 @@ describe('QueryParsingService', () => {
       expect(result).toEqual({
         type: 'groupingAggregation',
         rawSQL,
-        aggregationColumns: ['total_sales'],
-        dimensionColumns: ['month'],
+        aggregationColumns: ['sum'],
+        dimensionColumns: ['date_trunc'],
       });
     });
   });
--- apps/server/src/app/service/query-parsing.service.ts
+++ apps/server/src/app/service/query-parsing.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import {
-  AggrFunc,
-  Cast,
-  Column,
-  ColumnRef,
-  Function,
-  Parser,
-} from 'node-sql-parser';
+// import {
+//   AggrFunc,
+//   Cast,
+//   Column,
+//   ColumnRef,
+//   Function,
+//   Parser,
+// } from 'node-sql-parser';
 import { isDeepStrictEqual } from 'util';
+import { parse } from 'pgsql-parser';
+import { Node, RawStmt, ResTarget } from '@pgsql/types';
 
 export type BasicAggregation = {
   type: 'basicAggregation';
@@ -24,6 +26,10 @@ export type SQLQuery = {
   rawSQL: string;
 } & (BasicAggregation | GroupingAggregation);
 
+export type ParseError = {
+  message: string;
+};
+
 /**
  * TODO
  * - With clause: WITH sales AS (SELECT * FROM products) SELECT * FROM sales;
@@ -33,39 +39,76 @@ export type SQLQuery = {
  */
 @Injectable()
 export class QueryParsingService {
-  private readonly parser = new Parser();
+  // private readonly parser = new Parser();
 
-  parse(rawSQL: string): SQLQuery | null {
-    const parseResult = this.parser.astify(rawSQL, { database: 'Postgresql' });
-    const ast = Array.isArray(parseResult) ? parseResult[0] : parseResult;
+  parse(rawSQL: string): SQLQuery | ParseError {
+    const stmts = parse(rawSQL);
 
-    if (ast.type !== 'select') {
-      return null;
+    if (stmts.length < 1) {
+      return {
+        message: 'No statement found',
+      };
     }
 
-    const columns = ast.columns as Column[];
+    const rawStmt: RawStmt | undefined = stmts[0]?.RawStmt;
 
-    if (!ast.groupby) {
+    if (!rawStmt?.stmt) {
       return {
-        ...this.basicAggregation(columns),
+        message: 'No statement found',
+      };
+    }
+
+    if (!('SelectStmt' in rawStmt.stmt)) {
+      return {
+        message: 'Only SELECT statements are supported',
+      };
+    }
+
+    const selectStmt = rawStmt.stmt.SelectStmt;
+    if (!selectStmt.groupClause) {
+      return {
+        ...this.basicAggregation(selectStmt.targetList ?? []),
         rawSQL,
       };
     } else {
       return {
-        ...this.groupingAggregation(columns, ast.groupby),
+        ...this.groupingAggregation(
+          selectStmt.targetList ?? [],
+          selectStmt.groupClause ?? [],
+        ),
         rawSQL,
       };
     }
   }
 
-  private basicAggregation(columns: Column[]): BasicAggregation {
-    const aggregationColumns = columns
-      .filter((c) => c.type === 'expr' && c.expr.type === 'aggr_func')
-      .map((c) => {
-        const aggFunction = c.expr as AggrFunc;
-        return c.as ? c.as.toString() : aggFunction.name;
+  private basicAggregation(targetList: Node[]): BasicAggregation {
+    const aggregationColumns = targetList
+      .filter((t): t is { ResTarget: ResTarget } => 'ResTarget' in t)
+      .map((t) => t.ResTarget)
+      .map((t) => {
+        if (t.val && 'FuncCall' in t.val) {
+          const alias = t.name;
+          const funcName = t.val.FuncCall.funcname?.[0];
+          const funcNameString =
+            funcName &&
+            'String' in funcName &&
+            'str' in funcName.String &&
+            typeof funcName.String.str === 'string'
+              ? funcName.String.str
+              : null;
+
+          if (
+            funcNameString &&
+            ['sum', 'avg', 'count', 'max', 'min'].indexOf(funcNameString) !== -1
+          ) {
+            return alias ?? funcNameString;
+          }
+        }
+        return null;
       })
+      .filter((agg): agg is string => agg !== null)
       .map((agg) => agg.toLowerCase());
+
     return {
       type: 'basicAggregation',
       aggregationColumns,
@@ -73,52 +116,111 @@ export class QueryParsingService {
   }
 
   private groupingAggregation(
-    columns: Column[],
-    groupBy: { columns: ColumnRef[] },
+    targetList: Node[],
+    groupClause: Node[],
   ): GroupingAggregation {
     const aggregationColumns =
-      this.basicAggregation(columns).aggregationColumns;
+      this.basicAggregation(targetList).aggregationColumns;
 
     const dimensionColumns: string[] = [];
-    for (const column of columns) {
-      if (column.expr.type === 'column_ref') {
-        const columnRef = column.expr as ColumnRef;
-        for (const groupByColumnRef of groupBy.columns) {
-          if (isDeepStrictEqual(columnRef, groupByColumnRef)) {
-            if (column.as) {
-              dimensionColumns.push(column.as.toString().toLowerCase());
-            } else if (typeof columnRef.column === 'string') {
-              dimensionColumns.push(columnRef.column.toLowerCase());
-            } else {
-              dimensionColumns.push(
-                columnRef.column.expr.value.toString().toLowerCase(),
-              );
+    for (const groupNode of groupClause) {
+      if ('ColumnRef' in groupNode) {
+        const groupColumnRef = groupNode.ColumnRef;
+        for (const selectNode of targetList) {
+          if (
+            'ResTarget' in selectNode &&
+            selectNode.ResTarget.val &&
+            'ColumnRef' in selectNode.ResTarget.val
+          ) {
+            const selectColumnRef = selectNode.ResTarget.val.ColumnRef;
+            if (
+              isDeepStrictEqual(groupColumnRef.fields, selectColumnRef.fields)
+            ) {
+              if (selectNode.ResTarget.name) {
+                dimensionColumns.push(selectNode.ResTarget.name);
+              } else {
+                const field = selectColumnRef.fields?.[0];
+                if (
+                  field &&
+                  'String' in field &&
+                  'str' in field.String &&
+                  typeof field.String.str === 'string'
+                ) {
+                  dimensionColumns.push(field.String.str);
+                }
+              }
+            }
+          }
+
+          if (
+            'ResTarget' in selectNode &&
+            selectNode.ResTarget.val &&
+            'FuncCall' in selectNode.ResTarget.val
+          ) {
+            const selectFuncCall = groupColumnRef.fields?.[0];
+            if (
+              selectFuncCall &&
+              'String' in selectFuncCall &&
+              'str' in selectFuncCall.String &&
+              typeof selectFuncCall.String.str === 'string' &&
+              selectNode.ResTarget.name === selectFuncCall.String.str
+            ) {
+              dimensionColumns.push(selectFuncCall.String.str);
+            }
+          }
+
+          if (
+            'ResTarget' in selectNode &&
+            selectNode.ResTarget.val &&
+            'TypeCast' in selectNode.ResTarget.val &&
+            selectNode.ResTarget.val.TypeCast.arg &&
+            'ColumnRef' in selectNode.ResTarget.val.TypeCast.arg
+          ) {
+            const selectColumnRef =
+              selectNode.ResTarget.val.TypeCast.arg.ColumnRef;
+
+            if (
+              isDeepStrictEqual(groupColumnRef.fields, selectColumnRef.fields)
+            ) {
+              if (selectNode.ResTarget.name) {
+                dimensionColumns.push(selectNode.ResTarget.name);
+              }
             }
           }
         }
       }
 
-      if (column.expr.type === 'cast') {
-        const cast = column.expr as Cast;
-        const columnRef = cast.expr as ColumnRef;
-        for (const groupByColumnRef of groupBy.columns) {
-          if (isDeepStrictEqual(columnRef, groupByColumnRef)) {
-            if (cast['as']) {
-              dimensionColumns.push(cast['as'].toString().toLowerCase());
-            } else if (typeof columnRef.column === 'string') {
-              dimensionColumns.push(columnRef.column.toLowerCase());
-            } else {
-              dimensionColumns.push(
-                columnRef.column.expr.value.toString().toLowerCase(),
-              );
+      if ('FuncCall' in groupNode) {
+        const groupFuncCall = groupNode.FuncCall;
+        for (const selectNode of targetList) {
+          if (
+            'ResTarget' in selectNode &&
+            selectNode.ResTarget.val &&
+            'FuncCall' in selectNode.ResTarget.val
+          ) {
+            const selectFuncCall = selectNode.ResTarget.val.FuncCall;
+            if (
+              isDeepStrictEqual(groupFuncCall.funcname, selectFuncCall.funcname)
+            ) {
+              if (selectNode.ResTarget.name) {
+                dimensionColumns.push(selectNode.ResTarget.name);
+              } else {
+                const funcName = selectFuncCall.funcname?.[0];
+                if (
+                  funcName &&
+                  'String' in funcName &&
+                  'str' in funcName.String &&
+                  typeof funcName.String.str === 'string'
+                ) {
+                  dimensionColumns.push(funcName.String.str);
+                }
+              }
             }
           }
         }
       }
 
-      if (column.expr.type === 'function') {
-        // not implemented
-      }
+      continue;
     }
 
     return {
@@ -128,3 +230,59 @@ export class QueryParsingService {
     };
   }
 }
+
+// private groupingAggregation(
+//   columns: Column[],
+//   groupBy: { columns: ColumnRef[] },
+// ): GroupingAggregation {
+//   const aggregationColumns =
+//     this.basicAggregation(columns).aggregationColumns;
+
+//   const dimensionColumns: string[] = [];
+//   for (const column of columns) {
+//     if (column.expr.type === 'column_ref') {
+//       const columnRef = column.expr as ColumnRef;
+//       for (const groupByColumnRef of groupBy.columns) {
+//         if (isDeepStrictEqual(columnRef, groupByColumnRef)) {
+//           if (column.as) {
+//             dimensionColumns.push(column.as.toString().toLowerCase());
+//           } else if (typeof columnRef.column === 'string') {
+//             dimensionColumns.push(columnRef.column.toLowerCase());
+//           } else {
+//             dimensionColumns.push(
+//               columnRef.column.expr.value.toString().toLowerCase(),
+//             );
+//           }
+//         }
+//       }
+//     }
+
+//     if (column.expr.type === 'cast') {
+//       const cast = column.expr as Cast;
+//       const columnRef = cast.expr as ColumnRef;
+//       for (const groupByColumnRef of groupBy.columns) {
+//         if (isDeepStrictEqual(columnRef, groupByColumnRef)) {
+//           if (cast['as']) {
+//             dimensionColumns.push(cast['as'].toString().toLowerCase());
+//           } else if (typeof columnRef.column === 'string') {
+//             dimensionColumns.push(columnRef.column.toLowerCase());
+//           } else {
+//             dimensionColumns.push(
+//               columnRef.column.expr.value.toString().toLowerCase(),
+//             );
+//           }
+//         }
+//       }
+//     }
+
+//     if (column.expr.type === 'function') {
+//       // not implemented
+//     }
+//   }
+
+//   return {
+//     type: 'groupingAggregation',
+//     aggregationColumns,
+//     dimensionColumns,
+//   };
+// }
