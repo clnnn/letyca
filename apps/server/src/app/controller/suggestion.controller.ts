3298362ab116c7a43d26bcc16627f6f31fc3bac1
Calin C <calin.chiper@tss-yonder.com>
2024-08-21 09:02:52 +0000

    refactor: Update suggestion controller to retrieve suggestions using DDLService

--- apps/server/src/app/controller/suggestion.controller.ts
+++ apps/server/src/app/controller/suggestion.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PrismaService } from '../data-access/prisma.service';
+import { b } from 'baml_client';
+import { DDLService } from '../service/ddl.service';
 
 @Controller('suggestions')
 export class SuggestionController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly ddl: DDLService,
+  ) {}
 
   @Get()
   async getAll(
@@ -25,7 +30,8 @@ export class SuggestionController {
     }
 
     if (!userRequest) {
-      return [];
+      const ddl = await this.ddl.retrieve(connection);
+      return (await b.GetSuggestionsByDDL(ddl)).suggestions;
     } else {
       // not implemented yet
       return [];
